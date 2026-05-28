google/perfetto

[Refactor] Move common UI components and utilities to ui/src/lynx_perf and fix LynxPerf multi-trace overlay issue
Summary of change:
Relocated shared UI components and utilities from 'ui/src/plugins/lynx_perf' to
'ui/src/lynx_perf' for better code organization. Additionally, fixed an issue
in the LynxPerf plugin where multiple Trace issues were overlapping incorrectly.

Changes include:
- Moved common UI components and utilities to a centralized directory.
- Fixed multi-trace overlay issue in LynxPerf plugin.

ui/src/plugins/lynx.element/index.ts
@@ -18,8 +18,6 @@
 
 import {Trace} from '../../public/trace';
 import {PerfettoPlugin} from '../../public/plugin';
-import {lynxPerfGlobals} from '../../frontend/lynx_perf/lynx_perf_globals';
-import {LYNX_PERF_ELEMENT_PLUGIN_ID} from '../../frontend/lynx_perf/constants';
 import {NUM, STR} from '../../trace_processor/query_result';
 import {
   findDeeplyNestedNodesRecursively,
@@ -30,10 +28,13 @@ import {
 import {LynxElement} from './types';
 import ElementManager from './element_manager';
 import {Engine} from '../../trace_processor/engine';
-import {LynxElementTrack} from './element_track';
-import {IssueSummary, IssueRank} from '../../frontend/lynx_perf/types';
 import {getArgs} from '../../components/sql_utils/args';
 import {asArgSetId} from '../../components/sql_utils/core_types';
+import {IssueRank, IssueSummary} from '../../lynx_perf/types';
+import {LynxElementIssueTrack} from './element_issue_track';
+import {LYNX_PERF_ELEMENT_PLUGIN_ID} from '../../lynx_perf/constants';
+import LynxPerf from '../lynx.perf';
+import {lynxPerfGlobals} from '../../lynx_perf/lynx_perf_globals';
 
 /**
  * Lynx Element Performance Analysis Plugin
@@ -42,6 +43,7 @@ import {asArgSetId} from '../../components/sql_utils/core_types';
  */
 export default class LynxElementPlugin implements PerfettoPlugin {
   static readonly id = LYNX_PERF_ELEMENT_PLUGIN_ID;
+  static readonly dependencies = [LynxPerf];
   /**
    * Tracks problematic element nodes by instance_id
    * Key: instance_id
@@ -96,24 +98,16 @@ export default class LynxElementPlugin implements PerfettoPlugin {
    * guarantee those tracks will have been added yet.
    */
   async onTraceLoad(ctx: Trace): Promise<void> {
-    // TODO: reset issue status in perf track when v49.x.
-    lynxPerfGlobals.resetIssueStatus();
-
     await this.getScreenSize(ctx);
     const domIssues = await this.getIssueData(ctx.engine);
     if (domIssues.length > 0) {
       lynxPerfGlobals.appendPerformanceIssue(domIssues);
-      const timeout = ctx.commands.hasCommand('lynx.PerformanceIssues#update')
-        ? 0
-        : 500;
-      setTimeout(() => {
-        ctx.commands.runCommand('lynx.PerformanceIssues#update');
-      }, timeout);
+      ctx.commands.runCommand('lynx.PerformanceIssues#update');
     }
 
     ctx.tracks.registerTrack({
       uri: LYNX_PERF_ELEMENT_PLUGIN_ID,
-      track: new LynxElementTrack(),
+      track: new LynxElementIssueTrack(),
       title: 'Lynx Element Issues',
     });
   }

lynx-family/lynx-trace

ui/src/plugins/lynx.element/index.ts
@@ -18,8 +18,6 @@
 
 import {Trace} from '../../public/trace';
 import {PerfettoPlugin} from '../../public/plugin';
-import {lynxPerfGlobals} from '../../frontend/lynx_perf/lynx_perf_globals';
-import {LYNX_PERF_ELEMENT_PLUGIN_ID} from '../../frontend/lynx_perf/constants';
 import {NUM, STR} from '../../trace_processor/query_result';
 import {
   findDeeplyNestedNodesRecursively,
@@ -31,10 +29,13 @@ import {LynxElement} from './types';
 import ElementManager from './element_manager';
 import {Engine} from '../../trace_processor/engine';
 import {TrackNode} from '../../public/workspace';
-import {LynxElementTrack} from './element_track';
-import {IssueSummary, IssueRank} from '../../frontend/lynx_perf/types';
 import {Args, ArgsDict, getArgs} from '../../components/sql_utils/args';
 import {asArgSetId} from '../../components/sql_utils/core_types';
+import {IssueRank, IssueSummary} from '../../lynx_perf/types';
+import {LynxElementIssueTrack} from './element_issue_track';
+import {LYNX_PERF_ELEMENT_PLUGIN_ID} from '../../lynx_perf/constants';
+import LynxPerf from '../lynx.perf';
+import {lynxPerfGlobals} from '../../lynx_perf/lynx_perf_globals';
 
 /**
  * Lynx Element Performance Analysis Plugin
@@ -43,6 +44,7 @@ import {asArgSetId} from '../../components/sql_utils/core_types';
  */
 export default class LynxElementPlugin implements PerfettoPlugin {
   static readonly id = LYNX_PERF_ELEMENT_PLUGIN_ID;
+  static readonly dependencies = [LynxPerf];
   /**
    * Tracks problematic element nodes by instance_id
    * Key: instance_id
@@ -130,24 +132,16 @@ export default class LynxElementPlugin implements PerfettoPlugin {
    * guarantee those tracks will have been added yet.
    */
   async onTraceLoad(ctx: Trace): Promise<void> {
-    // TODO: reset issue status in perf track when v49.x.
-    lynxPerfGlobals.resetIssueStatus();
-
     await this.getScreenSize(ctx);
     const domIssues = await this.getIssueData(ctx.engine);
     if (domIssues.length > 0) {
       lynxPerfGlobals.appendPerformanceIssue(domIssues);
-      const timeout = ctx.commands.hasCommand('lynx.PerformanceIssues#update')
-        ? 0
-        : 500;
-      setTimeout(() => {
-        ctx.commands.runCommand('lynx.PerformanceIssues#update');
-      }, timeout);
+      ctx.commands.runCommand('lynx.PerformanceIssues#update');
     }
 
     ctx.tracks.registerTrack({
       uri: LYNX_PERF_ELEMENT_PLUGIN_ID,
-      renderer: new LynxElementTrack(),
+      renderer: new LynxElementIssueTrack(),
     });
     ctx.defaultWorkspace.addChildInOrder(
       new TrackNode({