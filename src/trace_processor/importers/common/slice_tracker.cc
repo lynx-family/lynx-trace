google/perfetto

[Feature] add JSProfilePacket support and implement JSProfileModule parser
This commit includes the following changes:
1. Introduced JSProfilePacket to TracePacket for representing JavaScript CPU profile data.
2. Added JSProfileModule to parse and process JSProfilePacket.
3. The module handles merging profile fragments, parsing JSON, reconstructing node relationships, and emitting trace events.

src/trace_processor/importers/common/slice_tracker.cc
@@ -379,6 +379,14 @@ void SliceTracker::MaybeCloseStack(int64_t ts,
       continue;
     }
 
+    auto category =
+        context_->storage->GetString(ref.category().value_or(kNullStringId))
+            .c_str();
+    if (strcmp(category, "jsprofile") == 0 && incomplete_descendent) {
+      ref.set_dur(ts - ref.ts());
+      continue;
+    }
+
     if (incomplete_descendent) {
       PERFETTO_DCHECK(ts >= start_ts);
 

lynx-family/lynx-trace

src/trace_processor/importers/common/slice_tracker.cc
@@ -392,6 +392,14 @@ bool SliceTracker::MaybeCloseStack(int64_t new_ts,
       continue;
     }
 
+    auto category =
+        context_->storage->GetString(ref.category().value_or(kNullStringId))
+            .c_str();
+    if (strcmp(category, "jsprofile") == 0 && incomplete_descendent) {
+      ref.set_dur(new_ts - ref.ts());
+      continue;
+    }
+
     if (incomplete_descendent) {
       PERFETTO_DCHECK(new_ts >= start_ts);
 